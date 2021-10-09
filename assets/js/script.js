df74f2c313b2705f1871d84358bdddbcd744ffa6
taylorkeltgen <taylorkeltgen@gmail.com>
2021-10-09 17:59:32 -0500

    randomizer completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,77 @@
 // Assignment code here
-var randomFunction = {
-  lower: randomLowercase,
-  upper: randomUppercase,
-  num: randomNumber,
-  special: randomSpecialChar
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbol = ['!', '#', '$', '%', '&', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', "'", '^', '_', '`', '{', '|', '}', '~'];
+
+// Randomizer
+var randomLower = function(){
+  value = Math.floor(Math.random() * (26));
+  return lowerCase[value];
 }
-
-
-
-var generatePassword = function(lower, upper, num, special) {
-  console.log(randomFunction);
-  console.log(randomLowercase);
-  var length = window.prompt("How many characters do you want your password to be?  Choose a length of at least 8 characters and no more than 128 characters.");
-  console.log('Password length: ' + length);
-  var passwordLength = parseInt(length);
-  // Verify user input a valid password length
-  if(passwordLength <= 128 && passwordLength >= 8){
-    var confirmLower = window.confirm("Would you like the password to include Lowercases?");
-    var confirmUpper = window.confirm("Would you like the password to include Uppercases?");
-    var confirmNumber = window.confirm("Would you like the password to include Numbers?");
-    var confirmSpecial = window.confirm("Would you like the password to include Special Characters?");
-    var confirmAmount = confirmLower + confirmUpper + confirmNumber + confirmSpecial;
-    console.log('confirmAmount: ' + confirmAmount);
-    // var confirmArray = [{confirmLower}, {confirmUpper}, {confirmNumber}, {confirmSpecial}];
-    // console.log('confirmArray: ' + confirmArray);
-    var typeArray = [{lower}, {upper}, {num}, {special}];
-    console.log('typeArray: ' + typeArray);
-    // Verify user selected one criteria option
-    if(confirmAmount < 1){
-      invalidOption();
-      generatePassword();
-    }
-    if(confirmLower = true){
-      return randomFunction.lower;
-    }
-
-  }
-  else {
-    invalidOption();
-    generatePassword();
-  }
-};
-
-
-
-// Prompt User to Re-enter a selection
-var invalidOption = function(){
-  window.alert("Please enter a valid number!");
-}
-
-// Generates Random Characters
-var randomLowercase = function(){
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-var randomUppercase = function(){
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+var randomUpper = function(){
+  value = Math.floor(Math.random() * (26));
+  return upperCase[value];
 }
 var randomNumber = function(){
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  value = Math.floor(Math.random() * (10));
+  return number[value];
 }
-var randomSpecialChar = function(){
-  var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-  return symbols[Math.floor(Math.random() * symbols.length)]
+var randomSymbol = function(){
+  value = Math.floor(Math.random() * (29));
+  return symbol[value];
 }
 
+/// PASSWORD GENERATOR FUNCTION START ///
+var generatePassword = function() {
+  var newPassword = "";
+  length = passwordLength();
+  // Verify user input a valid password length
+  if(length > 128 || length < 8 || length === " "){
+    window.alert("Please enter a valid number!");
+    generatePassword();
+  }
+  
+  // Prompt users for Character Criteria
+  var confirmLower = window.confirm("Would you like the password to include Lowercases?");
+  var confirmUpper = window.confirm("Would you like the password to include Uppercases?");
+  var confirmNumber = window.confirm("Would you like the password to include Numbers?");
+  var confirmSpecial = window.confirm("Would you like the password to include Special Characters?");
 
+  // Verify user selected atleast one criteria option, otherwise restart
+  var confirmAmount = confirmLower + confirmUpper + confirmNumber + confirmSpecial;
+  if(confirmAmount < 1){
+    window.alert("Please select atleast one character type!");
+    generatePassword();
+  }
+  /// build the password by looping ///
+  for(var i = 0; i < length; i++) {
+    if(confirmLower){
+    newPassword += randomLower();
+    }
+    if(confirmUpper){
+    newPassword += randomUpper();
+    }
+    if(confirmNumber){
+    newPassword += randomNumber();
+    }
+    if(confirmSpecial){
+    newPassword += randomSymbol();
+    }
+    if(i > length){
+      break;
+    }
+  }
+  console.log(newPassword.slice(0, length));
+  return newPassword.slice(0, length);
+};
+
+var passwordLength = function(){
+  var length = window.prompt("How many characters do you want your password to be?  Choose a length of at least 8 characters and no more than 128 characters.");
+    console.log('Password length: ' + length);
+  var getPasswordLength = parseInt(length);
+  return getPasswordLength;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
